e3f6ded1c41cf70d76bf5bfb83c484dd0d576345
Jules Lamur <contact@juleslamur.fr>
2017-12-27 14:51:45 +0100

    Add tp2 source code (main function).
    Edited to fix ANSI violations.

--- src/tp2.cpp
+++ src/tp2.cpp
@@ -1,10 +1,42 @@
+#include <cstdlib>
 #include <iostream>
+#include <vector>
+#include <fstream>
 
-int main(int argc, char** argv)
+typedef struct
 {
-    for (int i = 0; i < argc; i++) {
-        std::cout << argv[i] << std::endl;
+    int abs;
+    int ord;
+} coord;
+
+int main()
+{
+    int n;
+
+    std::cout << "Entrer le nombre de points: ";
+    std::cin >> n;
+
+    int m = n * (n - 1) / 2;
+
+    coord *points = new coord[n];
+    int **edge = new int*[m];
+    for (int i = 0; i < m; i++) {
+        edge[i] = new int[3];
     }
+    int **arbre = new int*[n - 1];
+    for (int i = 0; i < n - 1; i++) {
+        arbre[i] = new int[2];
+    }
+
+    delete[] points;
+    for (int i = 0; i < m; i++) {
+        delete[] edge[i];
+    }
+    delete[] edge;
+    for (int i = 0; i < n - 1; i++) {
+        delete[] arbre[i];
+    }
+    delete[] arbre;
 
     return 0;
 }
